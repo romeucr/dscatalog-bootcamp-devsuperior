6543bcb4653431ea621d6827449c9d3125f84e5f
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-10-01 15:40:31 +0200

    Audit data for Category

--- backend/src/main/java/com/rcrdev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rcrdev/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rcrdev.dscatalog.dto.CategoryDTO;
 import com.rcrdev.dscatalog.entities.Category;
 import com.rcrdev.dscatalog.repositories.CategoryRepository;
+import com.rcrdev.dscatalog.services.exceptions.DatabaseException;
 import com.rcrdev.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,8 +34,10 @@ public class CategoryService {
 
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
-		Optional<Category> obj = repository.findById(id); // findById returns an Optional.	
-		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found")); // to get as Category the Optional object
+		Optional<Category> obj = repository.findById(id); // findById returns an Optional.
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found")); // to get as
+																									// Category the
+																									// Optional object
 		return new CategoryDTO(entity);
 	}
 
@@ -46,17 +51,26 @@ public class CategoryService {
 
 	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO dto) {
-		
+
 		try {
-		Category entity = repository.getOne(id); // .getOne() dont touch the DB. Instantiates a provisory object with that ID. Just touch the DB when to save
-		entity.setName(dto.getName());
-		entity = repository.save(entity);
-		return new CategoryDTO(entity);
+			Category entity = repository.getOne(id); // .getOne() dont touch the DB. Instantiates a provisory object
+														// with that ID. Just touch the DB when to save
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found:" + id);
 		}
 	}
-	
-	
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found.");
+		} catch (DataIntegrityViolationException e) { // integrity. in case that there are products with the category, cant be deleted
+			throw new DatabaseException("Integrity violaton.");
+		}
+	}
 
 }
