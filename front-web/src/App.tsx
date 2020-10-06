f95f401ee3dbe186b4a840ae23dc06aea1f3739b
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-10-06 15:22:28 +0200

    Add NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss'
+import NavBar from './core/components/NavBar';
+
 
 const App = () => {
    return (
-      <h1>App</h1>
+      <NavBar />
    )
 }
 
