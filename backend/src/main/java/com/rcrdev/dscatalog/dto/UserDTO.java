bf800407c50a67caba2b81feed33ebdcb13b7dd8
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-10-26 16:23:43 +0100

    Basic bean validation

--- backend/src/main/java/com/rcrdev/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/rcrdev/dscatalog/dto/UserDTO.java
@@ -4,14 +4,20 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.rcrdev.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@NotBlank(message = "Campo obrigatorio")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Informar e-mail válido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
--- backend/src/main/java/com/rcrdev/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/rcrdev/dscatalog/resources/UserResource.java
@@ -2,6 +2,8 @@ package com.rcrdev.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -50,7 +52,7 @@ public class UserResource {
 	}
 
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
 		UserDTO newDto = service.insert(dto);
 
 		// to create the URI of the new object created. The location of the new object
@@ -60,7 +62,7 @@ public class UserResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
