3954cce81c894dacccb8f9c37a4981eb7a976dd7
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-10-27 15:38:25 +0100

    Custom Validation Response

--- backend/src/main/java/com/rcrdev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/rcrdev/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -44,4 +46,24 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError();
+		
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //código http 422
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("validation exception");
+		err.setMessage(e.getMessage()); 
+		err.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) { // .getBindResult recupera os erros específicos na eexcecao. .getFieldErrors retorna a lista com os campos dos erros gerados
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
 }
