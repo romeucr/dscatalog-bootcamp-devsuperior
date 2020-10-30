2b48fee78a1c7305d39c40a18042eadb96d8713d
Romeu Castro Rodrigues <romeu.castro@gmail.com>
2020-10-30 16:08:08 +0100

    h2 security configuration for test profile

--- backend/src/main/java/com/rcrdev/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/rcrdev/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.rcrdev.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,11 +16,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+	//para liberar a rota do h2-console. environment é o ambiente de execucao da aplicacao. Com ele é possivel acessar varias informacoes
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
 	//Declaracao de constantes para definicao das rotas e nivel de acesso
-	private static final String[] PUBLIC = { "/oauth/token" };
+	private static final String[] PUBLIC = { "/oauth/token" , "/h2-console/**"};
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 	private static final String [] ADMIN = {"/users/**"};
 	
@@ -30,6 +37,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	//configura as rotas que devem ter privilegio de acesso.
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+
+		//verifica o perfil de execucao da aplicacao e se for test, libera o acesso ao h2-console
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		
 		http.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll() //permite o acesso as rotas de PUBLIC a qualquer um
